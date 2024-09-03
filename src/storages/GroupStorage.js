719ee897b7a22c35e375765c5e2dc21e9f47d718
ppyom <ouob77@kakao.com>
2024-09-03 13:21:23 +0900

    feat: Group DefaultValue 수정

--- src/storages/GroupStorage.js
+++ src/storages/GroupStorage.js
@@ -1,14 +1,21 @@
 class GroupStorage {
   static #STORAGE_KEY = 'groups';
-  static #DEFAULT_VALUE = '{}';
+  static #DEFAULT_VALUE = `{
+      "1":{"id":"1","value":"가족"},
+      "2":{"id":"2","value":"친구"},
+      "3":{"id":"3","value":"직장"},
+      "4":{"id":"4","value":"스터디"}
+    }`;
   static #value = GroupStorage.#getFromStorage();
   static #getFromStorage() {
-    return JSON.parse(
+    const value = JSON.parse(
       localStorage.getItem(this.#STORAGE_KEY) || this.#DEFAULT_VALUE,
     );
+    this.#saveToStorage(value);
+    return value;
   }
-  static #saveToStorage() {
-    localStorage.setItem(this.#STORAGE_KEY, JSON.stringify(this.#value));
+  static #saveToStorage(value = this.#value) {
+    localStorage.setItem(this.#STORAGE_KEY, JSON.stringify(value));
   }
 
   static getList() {
