3a6c572758b286c0772238733934223730b5bb81
ppyom <ouob77@kakao.com>
2024-09-03 12:42:59 +0900

    feat: Search 기능 구현

--- src/components/ListCon/ListCon.jsx
+++ src/components/ListCon/ListCon.jsx
@@ -1,28 +1,37 @@
-import styles from './ListCon.module.css';
+import { useState } from 'react';
+import ContactListStorage from '../../storages/ContactListStorage.js';
 import Search from './Search/Search.jsx';
 import ListItem from './ListItem/ListItem.jsx';
-import ContactListStorage from '../../storages/ContactListStorage.js';
+import styles from './ListCon.module.css';
 
 const ListCon = ({ contactList, openDetailModal, setContactList }) => {
+  const [filter, setFilter] = useState('');
   const removeContact = (id) => {
     setContactList((prev) => prev.filter((item) => item.id !== id));
     ContactListStorage.remove(id);
   };
   return (
     <div className={styles.listCon}>
-      <Search />
+      <Search setFilter={setFilter} />
       <ul className={styles.list}>
-        {contactList.map((item) => (
-          <ListItem
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            phone={item.phone}
-            group={item.group}
-            openDetailModal={openDetailModal}
-            removeContact={removeContact}
-          />
-        ))}
+        {contactList
+          .filter(
+            (item) =>
+              item.name.includes(filter) ||
+              item.phone.includes(filter) ||
+              item.record.includes(filter),
+          )
+          .map((item) => (
+            <ListItem
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              phone={item.phone}
+              group={item.group}
+              openDetailModal={openDetailModal}
+              removeContact={removeContact}
+            />
+          ))}
       </ul>
     </div>
   );
--- src/components/ListCon/Search/Search.jsx
+++ src/components/ListCon/Search/Search.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import styles from './Search.module.css';
 
-const Search = () => {
+const Search = ({ setFilter }) => {
+  const [searchText, setSearchText] = useState('');
+
+  const updateFilter = (text = '') => {
+    setFilter(text);
+    setSearchText(text);
+  };
+  const handleEnterKeyUp = (event) =>
+    event.key === 'Enter' && updateFilter(searchText);
+
   return (
     <div className={styles.search}>
-      <input placeholder="검색어를 입력 후 엔터를 누르세요" />
-      <button className="btn primary">전체리스트 보기</button>
+      <input
+        placeholder="검색어를 입력 후 엔터를 누르세요"
+        value={searchText}
+        onChange={({ target }) => setSearchText(target.value)}
+        onKeyUp={handleEnterKeyUp}
+      />
+      <button className="btn primary" onClick={() => updateFilter()}>
+        전체리스트 보기
+      </button>
     </div>
   );
 };
